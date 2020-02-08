8ed7051f9e8753ae5d721f9f5f8d1a53d99fd5b8
Patrick Latorre <ptrckltrr@gmail.com>
2020-02-09 00:11:03 +0800

    Add purgecss to postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,18 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
+
 module.exports = {
     theme: {},
     variants: {},
     plugins: [
-        require('tailwindcss')
+        require('tailwindcss'),
+        cssnano({
+            preset: 'default'
+        }),
+        purgecss({
+            content: ['./client/src/index.html'],
+            css: ['./client/src/css/styles.css'],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+        })
     ]
 }
